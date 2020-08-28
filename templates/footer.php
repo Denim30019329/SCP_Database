a21d823abb6c3dbfe2a6ef59384d0d720bc403f7
Denim30019329 <denimfedermcdonald@gmail.com>
2020-08-28 14:16:15 +1200

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,8 @@
+<br>
+<br>
+<div class="fixed-bottom bg-dark text-light container-fluid">
+    <br>
+    <p class="float-right">Denim Feder-McDonald &copy</p>
+    <br>
+    <br>
+</div>
---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<div class="text-light bg-dark sticky-top w-100">
+    <div class="container-fluid pt-2 d-flex">
+        <div class="">
+            <img style="width:80px;" class="image-fluid pt-1" src="images/logo/Logo_of_the_SCP_Foundation.png" alt="" />
+        </div>
+        <div class="container-fluid pl-4" style="font-family: 'Volkhov', serif;">
+            <h1 class="remove-500">SCP Foundation</h1>
+            <h6 class="remove-500">Secure, Contain, Protect</h6>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-sm navbar-dark d-flex">
+        <a class="navbar-brand" href="index.php">SCP Files</a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="create.php">Add New Item</a>
+            </li>
+        </ul>
+    </nav>
+</div>
